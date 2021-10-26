b089b45ae7c47d2a5b0196f73b480bff75239481
LucasCegielski <lucas.cegielski@yahoo.ie>
2021-10-26 22:51:44 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JoxFqGoeeWDvCottDkwXeNMbZCbteAZlT2DfKhzvif6idtPEYhX2n78TvUxwNOVlfPkV97rOfQyUCcZmXtnmDIb00QuizfvaQ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
